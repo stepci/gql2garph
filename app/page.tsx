cef771a0693f15d0388577dd991f89af285cf6b3
Mish Ushakov <mishushakov@users.noreply.github.com>
2023-06-05 22:40:20 +0200

    Added logo

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import { generateCode } from './../lib'
 import { parse } from 'graphql'
 import Image from 'next/image'
 import Link from 'next/link'
+import logo from './logo.svg'
 
 export default function Home() {
   const [ts, setTS] = useState('')
@@ -31,21 +32,8 @@ export default function Home() {
     <main className="flex flex-col h-full">
       <div className="py-3 px-4 border-b bg-gray-100">
         <div className="flex items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z"
-            />
-          </svg>
-          <div className="ml-4 flex flex-1 flex-col">
+          <Image height={40} width={50} alt="GraphQL to Garph" src={logo} />
+          <div className="ml-2 flex flex-1 flex-col">
             <h1 className="flex select-none font-medium text-base leading-5">
               <span>GraphQL</span>
               <span className="mx-1">⇢</span>
@@ -58,14 +46,14 @@ export default function Home() {
           <nav className="flex">
             <Link
               href="https://github.com/stepci/gql2garph/issues/new"
-              target='_blank'
+              target="_blank"
               className="flex ml-2 p-2 select-none cursor-pointer font-medium text-sm"
             >
               Feedback
             </Link>
             <Link
               href="https://garph.dev/docs"
-              target='_blank'
+              target="_blank"
               className="flex ml-2 p-2 px-4 bg-gradient-to-r from-gray-800 to-gray-900 text-white rounded-full select-none cursor-pointer text-sm"
             >
               Docs
